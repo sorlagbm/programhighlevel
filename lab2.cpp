64570b9d32c531e05af6bf35ef8bebd9d7a962fc
sorlagbm <60257879+sorlagbm@users.noreply.github.com>
2021-09-19 16:40:33 +0300

    сделано

--- lab2.cpp
+++ lab2.cpp
@@ -31,11 +31,10 @@ private:
 
     };
     vector<student> list;
-    us n;
 
 public:
-    journal() : n(0) { list = vector<student>(this->n); }
-    explicit journal(us n) { this->n = n; list = vector<student>(this->n);}
+    journal() { list = vector<student>(0); }
+    explicit journal(us n) { list = vector<student>(n);}
     string& get_fio(us c) { return this->list[c].fio; }
     us& get_gn(us c) { return this->list[c].gn; }
     us& get_m(us c) { return this->list[c].m; }
@@ -49,10 +48,9 @@ public:
     }
     journal operator=(const journal& obj) {
 
-        this->n = obj.n;
         this->list.clear();
-        this->list.resize(this->n);
-        for (int i = 0; i < this->n; i++) {
+        this->list.resize(obj.list.size());
+        for (int i = 0; i < this->list.size(); i++) {
 
             this->list[i].fio = obj.list[i].fio;
             this->list[i].gn = obj.list[i].gn;
@@ -64,7 +62,7 @@ public:
     }
     friend ostream& operator<<(ostream& file, const journal& obj) {
 
-        for (int i = 0; i < obj.n; i++) {
+        for (int i = 0; i < obj.list.size(); i++) {
 
             file << "N:" << i << " "
                 << "FIO:" << obj.list[i].fio << " "
@@ -75,17 +73,25 @@ public:
         return file;
 
     }
+    void add(string fio, us gn, us m) {
+
+        student* e = new student(fio, gn, m);
+        this->list.push_back(*e);
+        delete e;
+
+    }
+    void _erase() { this->list.erase(this->list.end() - 1); }
     void set_list(us i, string fio, us gn, us m) {this->list[i] = student(fio, gn, m); }
     void sort_list() { 
 
-        vector<string> t(this->n);
-        for (int i = 0; i < this->n; i++) {
+        vector<string> t(this->list.size());
+        for (int i = 0; i < this->list.size(); i++) {
 
             t[i] = this->list[i].fio;
 
         }
         sort(t.begin(), t.end()); 
-        for (int i = 0; i < this->n; i++) {
+        for (int i = 0; i < this->list.size(); i++) {
 
             this->list[i].fio = t[i];
 
@@ -100,19 +106,44 @@ int main() {
 
     int c;
     cin >> c;
-    journal j(c);
+    journal* j = new journal(c);
     for (int i = 0; i < c; i++) {
 
-        cin >> j.get_fio(i);
-        cin >> j.get_gn(i);
-        cin >> j.get_m(i);
+        cin >> j->get_fio(i);
+        cin >> j->get_gn(i);
+        cin >> j->get_m(i);
 
     }
-    cout << j << "\n";
+    cout << *j << "\n";
     j[c - 1];
-    j.sort_list();
-    cout << j << "\n";
+    j->sort_list();
+    cout << *j << "\n";
+    j->add("new_student", 0, 0);
+    cout << *j << "\n";
+    j->_erase();
+    cout << *j << "\n";
+    vector<journal> u(c);
+    for (int i = 0; i < c; i++) {
 
+        u[i] = journal(3);
+
+    }
+    for (int i = 0; i < c; i++) {
+
+        for (int j = 0; j < 3; j++) {
+
+            u[i].set_list(j, "new_student", 0, 0);
+
+        }
+
+    }
+    for (int i = 0; i < c; i++) {
+
+        cout << u[i] << "\n";
+
+    }
+    *j = u[0];
+    cout << *j << "\n";
     return 0;
 
 }
