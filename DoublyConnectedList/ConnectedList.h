dfb04bab4a007fe1ebd324619c3880b1ecbd4a82
sorlagbm <60257879+sorlagbm@users.noreply.github.com>
2021-11-23 16:33:53 +0300

    Create ConnectedList.h

---
+++ DoublyConnectedList/ConnectedList.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "Node.h"
+
+/*
+* Циклический список
+*
+* Node<int>* curr = new Node<int>(5443);
+* Node<int>* head = new Node<int>(54, curr);
+* cout << head->getData() << endl;
+* curr->insertAfter(head);
+* cout << curr->getData() << endl;
+* curr = curr->nextNode();
+* cout << curr->getData() << endl;
+*/
+
+template<class T>
+class ConnectedList {
+
+	friend class Node;
+
+private:
+
+
+
+public:
+
+
+};
