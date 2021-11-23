a9eece7657e7ae01d19f1d29c28672ffff12a4dc
sorlagbm <60257879+sorlagbm@users.noreply.github.com>
2021-11-24 00:41:00 +0300

    Update ConnectedList.h

--- DoublyConnectedList/ConnectedList.h
+++ DoublyConnectedList/ConnectedList.h
@@ -1,28 +1,31 @@
 #pragma once
 #include "Node.h"
-
-/*
-* Циклический список
-*
-* Node<int>* curr = new Node<int>(5443);
-* Node<int>* head = new Node<int>(54, curr);
-* cout << head->getData() << endl;
-* curr->insertAfter(head);
-* cout << curr->getData() << endl;
-* curr = curr->nextNode();
-* cout << curr->getData() << endl;
-*/
-
 template<class T>
 class ConnectedList {
-
-	friend class Node;
-
+	friend class Node<T>;
 private:
 
+	Node<T>* head, *tail;
+	Node<T>* prev, *curr, *next;
 
+	size_t lenght;
+	int pos;
 
 public:
 
+	ConnectedList() : head(NODE_NULL), tail(NODE_NULL), prev(NODE_NULL)
+		, curr(NODE_NULL), next(NODE_NULL), lenght(0), pos(-1) {}
+	
+	size_t size() const { return this->lenght; }
+	bool isEmpty() const { return this->lenght == 0 ? true : false; }
+	int position() const { return this->pos; }
+	T& data() { return this->curr->data; }
+
+	void reset(int pos = 0);
+	void _Next();
+	void _Back();
+	bool isEndOfList() const { return this->curr->next == NODE_NULL ? false : true; }
+
+	void clear();
 
 };
