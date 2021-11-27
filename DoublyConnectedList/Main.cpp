7b898803245806f0710c51f3e9b4440eedb07d51
sorlagbm <60257879+sorlagbm@users.noreply.github.com>
2021-11-27 19:15:35 +0300

    Update Main.cpp

--- DoublyConnectedList/Main.cpp
+++ DoublyConnectedList/Main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include "ConnectedList.h"
+#include "Money.h"
+#include "Date.h"
+#include <string>
 using namespace std;
 
 int main() {
-
-	ConnectedList<int> list;
-
+	
+	ConnectedList<int> list(256);
+	for (int i = 0; i < 5; i++) {
+		list.insertAfter(NEW(int, i));
+	}
+	for (int i = 0; i < 5; i++) {
+		cout << list.data() << " ";
+		list._next();
+	}
+	list.deleteNode(2);
+	list.reset();
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		cout << list.data() << " ";
+		list._next();
+	}
 	return 0;
 }
